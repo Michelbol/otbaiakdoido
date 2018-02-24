e3c6dfb5c461358e1be237da87be1b2d886df270
Michel <michel.bolzon123@gmail.com>
2018-02-24 11:31:41 -0300

    offline training

--- data/actions/scripts/offtrain_statue.lua
+++ data/actions/scripts/offtrain_statue.lua
@@ -3,23 +3,23 @@
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 
-if(item.itemid == 18488) then -- sword
+if(item.itemid == 9306) then -- sword
 
 doCreatureSetStorage(cid, 62669, SKILL_SWORD)
 
-elseif(item.itemid == 18489) then -- axe
+elseif(item.itemid == 8836) then -- axe
 
 doCreatureSetStorage(cid, 62669, SKILL_AXE)
 
-elseif(item.itemid == 18492) then -- mlvl
+elseif(item.itemid == 8834) then -- mlvl
 
 doCreatureSetStorage(cid, 62669, SKILL__MAGLEVEL)
 
-elseif(item.itemid == 18490) then -- club
+elseif(item.itemid == 8626) then -- club
 
 doCreatureSetStorage(cid, 62669, SKILL_CLUB)
 
-elseif(item.itemid == 18491) then -- distannce
+elseif(item.itemid == 3739) then -- distannce
 
 doCreatureSetStorage(cid, 62669, SKILL_DISTANCE)
 
